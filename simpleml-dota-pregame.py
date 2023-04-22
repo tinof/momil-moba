525d7353a595b41ccd4bb732f6cb3adc9d4f9d41
Tino Fotiou <tino.fotiou@gmail.com>
2023-04-22 16:42:00 +0300

    Update simpleml-dota-pregame.py

--- simpleml-dota-pregame.py
+++ simpleml-dota-pregame.py
@@ -1,27 +1,27 @@
 import datetime
-
+import numpy as np
 import pandas as pd
 import requests
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from xgboost import XGBClassifier
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
 
-
-# Step 1: Fetch data from OpenDota API for all pro games played in the last 6 months
+# Fetch data from OpenDota API for all pro games played in the last 6 months
 def fetch_pro_matches():
     url = "https://api.opendota.com/api/proMatches"
     response = requests.get(url)
     return response.json()
 
 def filter_last_6_months(matches):
-    six_months_ago = datetime.datetime.now() - datetime.timedelta(days=6*30)
+    six_months_ago = datetime.datetime.now() - datetime.timedelta(days=4*30)
     timestamp_six_months_ago = six_months_ago.timestamp()
     return [match for match in matches if match['start_time'] > timestamp_six_months_ago]
 
 matches = fetch_pro_matches()
 filtered_matches = filter_last_6_months(matches)
 
-# Step 2: Process and prepare the data for modeling
+# Process and prepare the data for modeling
 def process_match_data(matches):
     processed_data = []
     for match in matches:
@@ -37,18 +37,30 @@ def process_match_data(matches):
 processed_matches = process_match_data(filtered_matches)
 df = pd.DataFrame(processed_matches)
 
-# Prepare data for training
 X = df.drop(['radiant_win', 'match_id'], axis=1)
 y = df['radiant_win']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Step 3: Train an XGBoost model to predict the outcome of future games
-model = XGBClassifier()
-model.fit(X_train, y_train)
+# Preprocess the data
+num_classes = len(np.unique(y_train))
+y_train_one_hot = to_categorical(y_train, num_classes)
+y_test_one_hot = to_categorical(y_test, num_classes)
 
-y_pred = model.predict(X_test)
-predictions = [round(value) for value in y_pred]
+input_dim = X_train.shape[1]
 
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy: %.2f%%" % (accuracy * 100.0))
+# Define the neural network model
+model = Sequential()
+model.add(Dense(64, input_dim=input_dim, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train_one_hot, epochs=10, batch_size=32, validation_split=0.2)
+
+# Evaluate the model on the test set
+score = model.evaluate(X_test, y_test_one_hot, verbose=0)
+print(f'Test loss: {score[0]}, Test accuracy: {score[1]}')
